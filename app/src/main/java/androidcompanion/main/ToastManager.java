18c6f8bf20e32d292deabbe6688889dc1ddde5fa
maxmaxmoser <maxmaxmoser@gmail.com>
2017-06-01 20:29:26 +0200

    Add ToastManager in order to makes Toast anywhere

---
+++ app/src/main/java/androidcompanion/main/ToastManager.java
@@ -0,0 +1,22 @@
+package androidcompanion.main;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+/**
+ * Created by Maxime on 01/06/2017.
+ */
+
+public class ToastManager {
+    public static Handler UIHandler = new Handler(Looper.getMainLooper());
+
+    public static void makeToast(final String toastMessage) {
+        UIHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MyApp.getContext(), toastMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
