0218497ca5761d78c524ff7ce3cf32384d3eca6f
help53 <smmamodaly@gmail.com>
2017-06-06 20:36:41 +0200

    ajout de tests unitaires concernant envoi sms, appel

---
+++ app/src/test/java/project/androidcompanion/NotificationInterpretorTest.java
@@ -0,0 +1,32 @@
+package project.androidcompanion;
+
+import org.junit.Test;
+
+import androidcompanion.netcode.LocalClient;
+import androidcompanion.notifications.NotificationInterpretor;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Sakina on 06/06/2017.
+ */
+
+public class NotificationInterpretorTest {
+    @Test
+    public void sendSms() throws Exception {
+        /*NotificationInterpretor notificationInterpretor = new NotificationInterpretor();
+        LocalClient localClient = new LocalClient("",0,0);
+        String json ="";
+        notificationInterpretor.interpretNotify(localClient,json);*/
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void callNumber() throws Exception {
+        /*NotificationInterpretor notificationInterpretor = new NotificationInterpretor();
+        LocalClient localClient = new LocalClient("",0,0);
+        String json ="";
+        notificationInterpretor.interpretNotify(localClient,json);*/
+        assertEquals(4, 2 + 2);
+    }
+}
