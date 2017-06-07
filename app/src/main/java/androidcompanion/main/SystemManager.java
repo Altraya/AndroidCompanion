3eca70083393e5b07b686201017826c80b53a21c
LookaSteve <senddevice@gmail.com>
2017-06-07 11:21:38 +0200

    Final Test

--- app/src/main/java/androidcompanion/main/SystemManager.java
+++ app/src/main/java/androidcompanion/main/SystemManager.java
@@ -4,11 +4,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.v4.content.LocalBroadcastManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import androidcompanion.data.SaveManager;
 import androidcompanion.netcode.Client;
 import androidcompanion.netcode.ClientManager;
+import androidcompanion.netcode.LocalClient;
 import androidcompanion.notifications.NotificationInterpretor;
 import androidcompanion.notifications.NotifyFactory;
 
@@ -61,8 +67,55 @@ public class SystemManager {
 
         if(context != null && !isNotificationListenerRegistered){
             LocalBroadcastManager.getInstance(instance.getContext()).registerReceiver(onNotice, new IntentFilter("Msg"));
+            init();
         }
 
+    }
+
+    private void init(){
+        // We copy the assets to the external storage
+        SharedPreferences settings = context.getSharedPreferences("PREFS_NAME", 0);
+        boolean isFirstLaunch = settings.getBoolean("FIRST_RUN", false);
+        if (!isFirstLaunch) {
+            // do the thing for the first time
+            // here we copy the assets to the external storage
+            SystemManager.getInstance().getSaveManager().copyAssets();
+            settings = context.getSharedPreferences("PREFS_NAME", 0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("FIRST_RUN", true);
+            editor.commit();
+        } else {
+            // other time the app loads
+        }
+
+        try
+        {
+            // reloading of the LocalClients list && connection
+            JSONObject jsonObj = new JSONObject(SystemManager.getInstance().getSaveManager().loadJSONFromAsset("device_list.json"));
+
+            if(!jsonObj.isNull("devices"))
+            {
+                JSONArray devices = jsonObj.getJSONArray("devices");
+                for(int i = 0; i < devices.length(); i++)
+                {
+                    JSONObject device = devices.getJSONObject(i);
+                    String deviceIPAdress = device.getString("ip_adress");
+                    String devicePort = device.getString("port");
+                    String devicePairingKey = device.getString("pairing_key");
+                    // Connection to the device using the infos previously provided
+                    LocalClient newClient = SystemManager.getInstance().getClientManager().addClient(deviceIPAdress,Integer.parseInt(devicePort),Integer.parseInt(devicePairingKey));
+                    // effective connection to the client (socket)
+                    if(newClient != null){
+                        newClient.connect();
+                    }
+                    //Toast.makeText(getApplicationContext(),"Device successfully connected!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     private BroadcastReceiver onNotice= new BroadcastReceiver() {
